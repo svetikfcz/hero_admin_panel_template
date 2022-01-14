bec65b59f29cbb472299b17dd1b37b1e5bf46257
Svetlana Efimova <svetlana.e08@gmail.com>
2022-01-14 15:15:20 +0300

    redux-thunk

--- src/components/heroesList/HeroesList.js
+++ src/components/heroesList/HeroesList.js
@@ -3,7 +3,7 @@ import { useEffect, useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { createSelector } from 'reselect';
 
-import { heroesFetching, heroesFetched, heroesFetchingError, heroDeleted } from '../../actions';
+import { fetchHeroes, heroDeleted } from '../../actions';
 import HeroesListItem from "../heroesListItem/HeroesListItem";
 import Spinner from '../spinner/Spinner';
 
@@ -40,10 +40,7 @@ const HeroesList = () => {
 
     //heroesFetching()
     useEffect(() => {
-        dispatch('HEROES_FETCHING');
-        request("http://localhost:3001/heroes")
-            .then(data => dispatch(heroesFetched(data)))
-            .catch(() => dispatch(heroesFetchingError()))
+        dispatch(fetchHeroes(request));
 
         // eslint-disable-next-line
     }, []);
