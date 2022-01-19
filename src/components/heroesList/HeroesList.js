30fe2cbfcd21a94519b48ea6b9b5bd88329e6ea9
Svetlana Efimova <svetlana.e08@gmail.com>
2022-01-19 21:26:19 +0300

    createAsyncThunk

--- src/components/heroesList/HeroesList.js
+++ src/components/heroesList/HeroesList.js
@@ -1,10 +1,9 @@
 import {useHttp} from '../../hooks/http.hook';
 import { useEffect, useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { createSelector } from 'reselect';
+import { createSelector } from '@reduxjs/toolkit';
 
-import { fetchHeroes } from '../../actions';
-import { heroDeleted } from './heroesSlice';
+import { heroDeleted, fetchHeroes } from './heroesSlice';
 import HeroesListItem from "../heroesListItem/HeroesListItem";
 import Spinner from '../spinner/Spinner';
 
@@ -41,7 +40,7 @@ const HeroesList = () => {
 
     //heroesFetching()
     useEffect(() => {
-        dispatch(fetchHeroes(request));
+        dispatch(fetchHeroes());
 
         // eslint-disable-next-line
     }, []);
